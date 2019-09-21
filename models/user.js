4ba7334cd4937b4b0b3c8a4e699234a6feaf1952
Bosco <nzibosco@gmail.com>
2019-09-21 12:22:04 -0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,73 @@
+module.exports = function(sequelize, DataTypes) {
+  const Menu = sequelize.define("Menu", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    email: {
+      type: DataTypes.email,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    streetAddress: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    // // check AWS for saving images @ Amazon and then use AWS links to store the images in our local db
+    // image: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1, 200]
+    //   }
+    // },
+    // detail: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1, 100]
+    //   }
+    // },
+    // price: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false
+    //   // validate: {
+    //   //   len: [1, 10]
+    //   // }
+    // }
+  });
+
+  return Menu;
+};
+
